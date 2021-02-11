20ab660e431a6c72937398ae7c0206e72edb24cd
Tobias Schlatter <tobias@meisch.ch>
2021-02-11 13:11:28 +0100

    Lighting somewhat works

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ extern crate image;
 extern crate quaternion;
 extern crate vecmath;
 
-use image::{GenericImage, Rgb, RgbImage};
+use image::{GenericImage, Pixel, Rgb, RgbImage};
 use vecmath::traits::Float;
 use vecmath::Vector3;
 
+use std::convert::TryInto;
 use std::option::Option;
 
 struct Ray<T> {
@@ -22,6 +23,11 @@ struct Poly<T, C> {
 struct Plane<T> {
     n: Vector3<T>,
     d: T,
+}
+
+struct Hit<T> {
+    point: Vector3<T>,
+    dist: T,
 }
 
 impl<T: Float, C> Poly<T, C> {
@@ -46,7 +52,7 @@ impl<T: Float, C> Poly<T, C> {
         };
     }
 
-    fn hit(&self, ray: &Ray<T>) -> Option<T> {
+    fn hit(&self, ray: &Ray<T>) -> Option<Hit<T>> {
         let n = self.plane.n;
 
         let dot = vecmath::vec3_dot(n, ray.dir);
@@ -77,7 +83,7 @@ impl<T: Float, C> Poly<T, C> {
             }
         }
 
-        return Some(d);
+        return Some(Hit { point: p, dist: d });
     }
 }
 
@@ -90,10 +96,94 @@ struct Camera<T> {
 
 struct Scene<T, C> {
     polys: Vec<Poly<T, C>>,
-    background: C,
+    dark: C,
+    bright: C,
+    light: Vector3<T>,
 }
 
-const TRACE_DEPTH: u16 = 2;
+struct Tracer<T> {
+    all_dirs: Vec<Vector3<T>>,
+    max_depth: u32,
+}
+
+impl<T: Float> Tracer<T> {
+    fn new(rays: u32, max_depth: u32) -> Tracer<T> {
+        let step = T::_360() / T::from_u32(rays);
+
+        let u = [T::one(), T::zero(), T::zero()];
+
+        let mut all_dirs = Vec::with_capacity((rays * rays).try_into().unwrap());
+
+        for x in 0..rays {
+            for y in 0..rays {
+                let q = quaternion::euler_angles(
+                    T::from_u32(x) * step,
+                    T::from_u32(y) * step,
+                    T::zero(),
+                );
+                all_dirs.push(quaternion::rotate_vector(q, u));
+            }
+        }
+
+        return Tracer {
+            all_dirs,
+            max_depth,
+        };
+    }
+
+    fn trace<C: Pixel<Subpixel = T>>(&self, scene: &Scene<T, C>, ray: &Ray<T>, depth: u32) -> C {
+        if depth > self.max_depth {
+            return scene.dark;
+        }
+
+        let (hit, poly) = match shoot(scene, ray) {
+            None => {
+                return if vecmath::vec3_dot(ray.dir, scene.light) > T::from_f64(0.4) {
+                    scene.bright
+                } else {
+                    scene.dark
+                };
+            }
+            Some(hit) => hit,
+        };
+
+        let mut all_light = scene.dark;
+
+        for dir in self.all_dirs.iter() {
+            let v = vecmath::vec3_dot(*dir, poly.plane.n);
+
+            if v == T::zero() {
+                // Perpendicular to surface.
+                continue;
+            }
+
+            if vecmath::vec3_dot(ray.dir, poly.plane.n) / v < T::zero() {
+                // Rays are not on the same side of the surfce.
+                continue;
+            }
+
+            let r = Ray {
+                // HACK: Move away to not hit same poly.
+                orig: vecmath::vec3_add(hit.point, vecmath::vec3_scale(*dir, T::from_f64(0.01))),
+                dir: *dir,
+            };
+
+            let lambert = {
+                if v < T::zero() {
+                    -v
+                } else {
+                    v
+                }
+            };
+
+            let light = self.trace(scene, &r, depth + 1).map(|c| c * lambert);
+
+            all_light = all_light.map2(&light, |x, y| x + y);
+        }
+
+        return all_light;
+    }
+}
 
 fn main() {
     let mut img = RgbImage::new(1001, 601);
@@ -102,22 +192,24 @@ fn main() {
         polys: vec![
             Poly::new(
                 [[2.0, 1.0, -8.0], [0.0, 0.0, -10.0], [-1.0, 1.0, -9.0]],
-                Rgb([255, 0, 0]),
+                Rgb([1.0, 0.0, 0.0]),
             ),
             Poly::new(
                 [[1.0, 1.0, -12.0], [0.0, 3.0, -8.0], [-3.0, -3.0, -8.0]],
-                Rgb([0, 0, 255]),
+                Rgb([0.0, 0.0, 1.0]),
             ),
             Poly::new(
                 [[2.0, 0.0, -8.0], [2.0, 0.0, -15.0], [1.5, -3.0, -15.0]],
-                Rgb([0, 255, 0]),
+                Rgb([0.0, 1.0, 0.0]),
             ),
             Poly::new(
                 [[-2.0, -1.0, -2.0], [-1.0, 2.0, -12.0], [1.5, -2.0, -5.0]],
-                Rgb([255, 255, 0]),
+                Rgb([1.0, 1.0, 0.0]),
             ),
         ],
-        background: Rgb([0, 0, 0]),
+        dark: Rgb([0.0, 0.0, 0.0]),
+        bright: Rgb([255.0, 255.0, 255.0]),
+        light: vecmath::vec3_normalized([1.0, 1.0, 1.0]),
     };
 
     let front = Camera {
@@ -148,10 +240,40 @@ fn main() {
         aperture: 30.0 / 180.0 * std::f64::consts::PI, // deg
     };
 
-    render(&scene, &front, &mut img.sub_image(0, 0, 500, 300));
-    render(&scene, &back, &mut img.sub_image(0, 301, 500, 300));
-    render(&scene, &right, &mut img.sub_image(501, 0, 500, 300));
-    render(&scene, &left, &mut img.sub_image(501, 301, 500, 300));
+    let tracer = Tracer::<f64>::new(5, 2);
+
+    let gamma = |c: Rgb<f64>| -> Rgb<u8> {
+        *Rgb::from_slice(&c.channels().iter().map(|x| *x as u8).collect::<Vec<u8>>())
+    };
+
+    render(
+        &tracer,
+        &scene,
+        &front,
+        gamma,
+        &mut img.sub_image(0, 0, 500, 300),
+    );
+    render(
+        &tracer,
+        &scene,
+        &back,
+        gamma,
+        &mut img.sub_image(0, 301, 500, 300),
+    );
+    render(
+        &tracer,
+        &scene,
+        &right,
+        gamma,
+        &mut img.sub_image(501, 0, 500, 300),
+    );
+    render(
+        &tracer,
+        &scene,
+        &left,
+        gamma,
+        &mut img.sub_image(501, 301, 500, 300),
+    );
 
     for i in 0..1001 {
         img.put_pixel(i, 300, Rgb([255, 255, 255]));
@@ -164,7 +286,13 @@ fn main() {
     img.save("test.png").unwrap();
 }
 
-fn render<F: Float, I: GenericImage>(scene: &Scene<F, I::Pixel>, camera: &Camera<F>, img: &mut I) {
+fn render<F: Float, C: Pixel<Subpixel = F>, I: GenericImage, G: Fn(C) -> I::Pixel>(
+    tracer: &Tracer<F>,
+    scene: &Scene<F, C>,
+    camera: &Camera<F>,
+    gamma: G,
+    img: &mut I,
+) {
     let (width, height) = img.dimensions();
     let center = vecmath::vec2_scale([F::from_u32(width), F::from_u32(height)], F::from_f64(0.5));
     let pix_ang = camera.aperture / F::from_u32(width);
@@ -187,25 +315,26 @@ fn render<F: Float, I: GenericImage>(scene: &Scene<F, I::Pixel>, camera: &Camera
                 dir: quaternion::rotate_vector(q, camera.dir),
             };
 
-            img.put_pixel(x, y, trace(scene, &r, TRACE_DEPTH));
+            let light = tracer.trace(scene, &r, 0);
+
+            img.put_pixel(x, y, gamma(light));
         }
     }
 }
 
-fn trace<F: Float, C: Copy>(scene: &Scene<F, C>, ray: &Ray<F>, _depth: u16) -> C {
-    return shoot(scene, ray).map_or(scene.background, |p| p.color);
-}
-
-fn shoot<'a, F: Float, C: Copy>(scene: &'a Scene<F, C>, ray: &Ray<F>) -> Option<&'a Poly<F, C>> {
-    let mut closest: Option<(F, &Poly<F, C>)> = None;
+fn shoot<'a, F: Float, C: Copy>(
+    scene: &'a Scene<F, C>,
+    ray: &Ray<F>,
+) -> Option<(Hit<F>, &'a Poly<F, C>)> {
+    let mut closest: Option<(Hit<F>, &Poly<F, C>)> = None;
 
     for p in &scene.polys {
-        if let Some(d) = p.hit(ray) {
-            if closest.map_or(true, |c| c.0 > d) {
-                closest = Some((d, &p));
+        if let Some(h) = p.hit(ray) {
+            if closest.as_ref().map_or(true, |c| c.0.dist > h.dist) {
+                closest = Some((h, &p));
             }
         }
     }
 
-    return closest.map(|p| p.1);
+    return closest;
 }
