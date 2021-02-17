58b67823419a0b334fb2dd191ed25d3e9e01b3f2
Tobias Schlatter <tobias@meisch.ch>
2021-02-17 16:30:00 +0100

    Create parallelograms instead of quadrilaterals

--- src/shapes.rs
+++ src/shapes.rs
@@ -5,7 +5,17 @@ use vecmath::Vector3;
 
 use crate::geom::Poly;
 
-pub fn add_quad<T: Float, S: Clone>(pts: [Vector3<T>; 4], surface: S, trg: &mut Vec<Poly<T, S>>) {
-    trg.push(Poly::new([pts[0], pts[1], pts[2]], surface.clone()));
-    trg.push(Poly::new([pts[0], pts[3], pts[2]], surface));
+pub fn add_par<T: Float, S: Clone>(
+    a: Vector3<T>,
+    b_side: Vector3<T>,
+    c_side: Vector3<T>,
+    surface: S,
+    trg: &mut Vec<Poly<T, S>>,
+) {
+    let b = vecmath::vec3_add(a, b_side);
+    let c = vecmath::vec3_add(a, c_side);
+    let d = vecmath::vec3_add(b, c_side);
+
+    trg.push(Poly::new([a, b, d], surface.clone()));
+    trg.push(Poly::new([a, c, d], surface));
 }
